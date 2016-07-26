e57d4dbb5f2f9b32abf035aba3390409b80e8697
jachhuo <1492452565@qq.com>
2016-07-26 15:20:54 +0800

    zhibo

---
+++ app/src/main/java/drawerlayout/tencent/com/zhibotv/MainActivity.java
@@ -0,0 +1,13 @@
+package drawerlayout.tencent.com.zhibotv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/drawerlayout/tencent/com/zhibotv/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package drawerlayout.tencent.com.zhibotv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
